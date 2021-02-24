7129abdff282249b8f1281482cbf62b116532f06
Alex Joliet <p21ajoliet@fairfieldprep.org>
2021-02-24 10:35:15 -0500

    made 3 work

--- word_play-3.py
+++ word_play-3.py
@@ -1,23 +1,16 @@
-# Write a program that contains a function called `avoid` that takes a word 
-# and a string of forbidden letters and returns `True` if the word does not 
-# use any of the forbidden letters. The program should prompt the user to enter a string 
-# of forbidden letters and use the function to print to the console the number of 
-# words in `words.txt` that don't contain any of the forbidden letters.
+#Author: ALJ (AMDG) 2/24/21
 
-def avoid(word, letters):
-    for y in letters:
-        if y not in word:
-            return True
+def avoid(word, forbidden):
+    for letter in word:
+        if letter in forbidden:
+            return False
+    return True
 
 fletters = input("Type the forbidden letters: ")
-fletters1 = []
-
-for x in fletters:
-    fletters1.append(x)
 
 with open("words.txt") as file1:
     words = file1.readlines()
 
     for line in words:
-        if avoid(line, fletters1):
+        if avoid(line, fletters):
             print(line)
